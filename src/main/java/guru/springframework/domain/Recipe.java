7a24d88b82ce22ef91765e46aa3bb01516730a33
Juan Quiroga <juanpquiroga@gmail.com>
2018-10-06 11:29:16 -0500

    Inclusion de relaciones uno a uno y one a muchos

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,5 +1,7 @@
 package guru.springframework.domain;
 
+import java.util.Set;
+
 import javax.persistence.*;
 
 /**
@@ -20,13 +22,16 @@ public class Recipe {
     private String url;
     private String directions;
     //todo add
-    //private Difficulty difficulty;
+    //private Difficulty difficulty;    
 
     @Lob
     private Byte[] image;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    
+    @OneToMany(cascade=CascadeType.ALL, mappedBy="recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
@@ -107,4 +112,12 @@ public class Recipe {
     public void setNotes(Notes notes) {
         this.notes = notes;
     }
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
 }
